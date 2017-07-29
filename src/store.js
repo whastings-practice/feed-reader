448735efdfaa1adb442ac8db7125ae9e99162e4d
Will Hastings <will@objectdotcreate.net>
2017-07-29 13:30:22 -0700

    Install redux-devtools-extension

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
-import { createStore as createReduxStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { createStore as createReduxStore, applyMiddleware, combineReducers } from 'redux';
 import { connectRoutes } from 'redux-first-router';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import routes from './routes';
 import createHistory from 'history/createBrowserHistory';
@@ -16,7 +17,7 @@ export default function createStore() {
 
   const rootReducer = combineReducers({ ...reducers, location: connectedRoutes.reducer });
   const middlewareEnhancer = applyMiddleware(connectedRoutes.middleware, sagaMiddleware);
-  const enhancers = compose(connectedRoutes.enhancer, middlewareEnhancer);
+  const enhancers = composeWithDevTools(connectedRoutes.enhancer, middlewareEnhancer);
 
   const store = createReduxStore(rootReducer, enhancers);
 
