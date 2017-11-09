5f95468569727a277b8b1517e600f859a4449bcb
Will Hastings <will@objectdotcreate.net>
2017-11-09 15:18:30 -0800

    Switch to using Bootstrap instead of Semantic UI

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import Link from 'redux-first-router-link';
-import { Container, Header, Menu } from 'semantic-ui-react';
+import Container from 'reactstrap/lib/Container';
+import Navbar from 'reactstrap/lib/Navbar';
+import NavbarBrand from 'reactstrap/lib/NavbarBrand';
+import Nav from 'reactstrap/lib/Nav';
+import NavItem from 'reactstrap/lib/NavItem';
+import NavLink from 'reactstrap/lib/NavLink';
 
 export default function Page(props) {
   return (
     <div>
-      <header>
-        <Menu size="massive" borderless>
-          <Container>
-            <Header as="h1">
-              <Link to="/">Read &apos;Dem Feeds!</Link>
-            </Header>
-          </Container>
-        </Menu>
-      </header>
+      <Navbar>
+        <Container>
+          <NavbarBrand tag="h1">
+            <Link to="/">Read &apos;Dem Feeds!</Link>
+          </NavbarBrand>
+          <Nav navbar>
+            <NavItem>
+              <NavLink tag={Link} to="/feeds">Feeds</NavLink>
+            </NavItem>
+          </Nav>
+        </Container>
+      </Navbar>
       <Container>
         {props.children}
       </Container>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import 'semantic-ui-css/semantic.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
